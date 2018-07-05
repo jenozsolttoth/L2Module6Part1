e033fd2cc93e5bed12a03d54b7a85014d9243534
zsto01 <jeno.zsolt.toth@jencysoft.com>
2018-07-05 20:13:32 +0200

    Added multiple strategy

---
+++ LoggerDemo/MultipleStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using L2Module6.Loggers;
+using L2Module6.Strategies;
+
+namespace LoggerDemo
+{
+    class MultipleStrategy : IPersistenceStrategy
+    {
+        FileStrategy fileStrategy = new FileStrategy("testmulti.txt",'^');
+        LiteDatabaseStrategy dbStrategy = new LiteDatabaseStrategy("testmulti");
+        ConsoleStrategy consoleStrategy = new ConsoleStrategy();
+        public void Store(LogObject storable)
+        {
+            fileStrategy.Store(storable);
+            dbStrategy.Store(storable);
+            consoleStrategy.Store(storable);
+        }
+    }
+}
--- LoggerDemo/Program.cs
+++ LoggerDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using L2Module6;
 using L2Module6.Factories;
 using L2Module6.Loggers;
+using L2Module6.Strategies;
 
 namespace LoggerDemo
 {
@@ -11,7 +12,7 @@ namespace LoggerDemo
         static void Main(string[] args)
         {
             var logger = Logger.Instance;
-            Logger.PersistenceStrategy = new ConsoleStrategy();
+            Logger.PersistenceStrategy = new MultipleStrategy();
             logger.Log(new LogObject(){Exception = new TimeoutException("sajtreszelo"), Level = LogLevel.Critical, Message = "No message", Method = "Method"});
         }
     }
